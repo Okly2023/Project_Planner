a76847e3308b817de8668bc7837374c8d8cb6a7d
v-alex-dev <vensalex1991@gmail.com>
2023-10-03 17:27:11 +0200

    add component card

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,27 @@
+import { newCard } from "./component/main/card/newCard.js";
 import { clock } from "./component/form-header/form-header-clock.js";
 import { formProject } from "./component/form-project/formProject.js";
 import { barNav } from "./component/navigation/bar-nav.js";
 import { addProject } from "./function/add-project.js";
 
+const div = document.querySelector('.form-project');
+const main = document.querySelector('main');
 
+const section = document.createElement('section');
+section.className = 'list-card';
+main.appendChild(section)
 formProject();
 addProject((data) => {
  	clock(data)
- 	const div = document.querySelector('.form-project');
+
+
 	if(div){
 		div.remove();
 	}
-});
 
+
+});	
+newCard('alex');
+newCard('alex');newCard('alex');newCard('alex');newCard('alex');newCard('alex');newCard('alex');
 
 barNav();
