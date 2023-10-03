82e4aefa5a0f0c05ff5c26a7a30171553e0e16b5
v-alex-dev <vensalex1991@gmail.com>
2023-10-03 15:28:35 +0200

    push for okly

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+import { clock } from "./component/form-header/form-header-clock.js";
 import { formProject } from "./component/form-project/formProject.js";
 import { barNav } from "./component/navigation/bar-nav.js";
 import { addProject } from "./function/add-project.js";
@@ -5,7 +6,12 @@ import { addProject } from "./function/add-project.js";
 
 formProject();
 addProject((data) => {
- 	console.log(data); // Les données du formulaire sont disponibles ici
+ 	clock(data)
+ 	const div = document.querySelector('.form-project');
+	if(div){
+		div.remove();
+	}
 });
 
 
+barNav();
