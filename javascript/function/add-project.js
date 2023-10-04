0acaf5eda1fc6de9fdd5405fefcc160d8acae0b0
v-alex-dev <vensalex1991@gmail.com>
2023-10-04 22:14:54 +0200

    add valid-date

--- javascript/function/add-project.js
+++ javascript/function/add-project.js
@@ -1,18 +1,60 @@
+import { validDate } from "./validate-input-date.js";
+import { validTxtInput } from "./validate-input-txt.js";
+
 export function addProject(callback) {
   const btn = document.getElementById('submit-project');
   const nameForm = document.getElementById('name-project');
   const descriptionForm = document.getElementById('description-project');
   const dateForm = document.getElementById('date-project-dead-line');
 
+  nameForm.addEventListener('keyup', (e) =>{
+    let value = e.target.value
+    let boolean = validTxtInput(value, 4, 15)
+
+    if(!boolean){
+      nameForm.style.backgroundColor = 'rgb(255,0 ,0, 0.2)'
+      nameForm.style.boxShadow = '0 0 15px rgb(255,0 ,0, 0.2)'
+    }else{
+      nameForm.style.backgroundColor = 'rgb(0,204,0, 0.2)'
+      nameForm.style.boxShadow = '0 0 15px rgb(0,204,0, 0.2)'
+    }
+  })
+  descriptionForm.addEventListener('keyup', (e) =>{
+    let value = e.target.value
+    let boolean = validTxtInput(value, 5, 200)
+
+    if(!boolean){
+      descriptionForm.style.backgroundColor = 'rgb(255,0 ,0, 0.2)'
+      descriptionForm.style.boxShadow = '0 0 15px red'
+    }else{
+      descriptionForm.style.backgroundColor = 'rgb(0,204,0, 0.2)'
+      descriptionForm.style.boxShadow = '0 0 15px rgb(0,204,0, 0.2)'
+    }
+  })
 
   btn.addEventListener('click', () => {
-    const formData = {
-      name: nameForm.value,
-      description: descriptionForm.value,
-      date: dateForm.value
-    };
-
-    // Appelez la fonction de rappel avec les données du formulaire
-    callback(formData);
+    let validName = validTxtInput(nameForm.value, 4,15);
+    let validDescri = validTxtInput(descriptionForm.value,5,200);
+    let dateValid = validDate(dateForm.value);
+    if (validName && validDescri && dateValid) {
+      const formData = {
+        name: nameForm.value,
+        description: descriptionForm.value,
+        date: dateForm.value
+      };
+      callback(formData);
+    }else{
+      if(!validName) {
+        nameForm.value = '';
+      }
+      if(!validDescri){
+        descriptionForm.value = '';
+      }
+      if(!dateValid){
+        dateForm.value = '';
+        alert(`La date doit être supérieur a celle d'aujourd'hui.`)
+      }
+      alert(`L'un des champs ne remplis pas les conditions.`)
+    }   
   });
 }
---
+++ javascript/function/validate-input-date.js
@@ -0,0 +1,11 @@
+/*	Function validate date description in FR
+*		Validation d'une date input. S'assurer que la date n'est pas inférieur a la date d'aujourd'hui.	
+*	
+*/
+export function validDate(date) {
+	const dateNow = new Date();
+	const dateProject = new Date(date)
+	console.log(dateNow);
+	console.log(dateProject);
+	return dateProject > dateNow ? true : false;
+}
